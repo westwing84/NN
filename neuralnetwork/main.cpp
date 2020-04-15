b12b9aca80b4218fd5fcfe58e91bd18d4b61f639
TsubasaNishihara <63089960+TsubasaNishihara@users.noreply.github.com>
2020-04-16 00:42:58 +0900

    Update neuralnetwork

--- neuralnetwork/main.cpp
+++ neuralnetwork/main.cpp
@@ -13,7 +13,10 @@
 #define INPUT 3		//入力の個数
 #define OUTPUT 1	//出力の個数
 
+//関数のプロトタイプ宣言
 double sigmoid(double s);
+double diff(double (*func)(double, double), double x, double t);
+double loss_func(double x, double t);
 void learning(double t_in[], double t_out[],
 	int input, int output, int layer, int elenum,
 	double epsilon
@@ -21,6 +24,7 @@ void learning(double t_in[], double t_out[],
 
 static double*** omega;	//重み
 
+//main関数
 int main(void) {
 	
 	int layer = LAYER, elenum = NUM, input = INPUT, output = OUTPUT;
@@ -31,18 +35,22 @@ int main(void) {
 	//omegaのデータ領域確保
 	omega = (double***)calloc(layer, sizeof(double));
 	for (int i = 0; i < layer; i++) {
-		omega[i] = (double**)calloc(elenum, sizeof(double));
-		for (int j = 0; j < elenum; j++) {
+		omega[i] = (double**)calloc(elenum + 1, sizeof(double));
+		for (int j = 0; j < elenum + 1; j++) {
 			omega[i][j] = (double*)calloc(elenum, sizeof(double));
 		}
 	}
+	//変数omegaの説明
+	//omega[i][j][k]: i層目のj番目のニューロンから(i+1)層目の(k+1)番目のニューロンへの枝の重み．
+	//0層目は入力層，0番目のニューロンはバイアスとする．
+
 
 	//重みの初期値を乱数により決定
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < layer; i++) {
-		for (int j = 0; j < elenum; j++) {
+		for (int j = 0; j < elenum + 1; j++) {
 			for (int k = 0; k < elenum; k++) {
-				omega[i][j][k] = rand() / (RAND_MAX + 1);
+				omega[i][j][k] = (double)rand() / (double)(RAND_MAX + 1);
 			}
 		}
 	}
@@ -61,9 +69,22 @@ int main(void) {
 	return 0;
 }
 
+
 //シグモイド関数
 double sigmoid(double s) {
 	return 1 / (1 + exp(-s));
+}
+
+//2変数関数funcのxにおける微分係数を求める関数
+double diff(double (*func)(double, double), double x, double t) {
+	double delta = 1e-4;
+	return (func(x + delta, t) - func(x - delta, t)) / (2 * delta);
+}
+
+//損失関数
+//x: ニューロンへの入力，t: 出力の教師データ
+double loss_func(double x, double t) {
+	return pow(sigmoid(x) - t, 2);
 }
 
 //教師データを学習させ，重みomegaを決定する．epsilonは学習率．
@@ -74,6 +95,10 @@ void learning(double t_in[], double t_out[],
 	double*** x = (double***)calloc(layer, sizeof(double));				//各ニューロンへの入力
 	double** u = (double**)calloc(layer - 1, sizeof(double));			//各ニューロンからの出力
 	double* y = (double*)calloc(output, sizeof(double));				//NNの出力
+	double b = 1;														//バイアス
+	double error;														//誤差
+	double** dLdx = (double**)calloc(layer - 1, sizeof(double));		//逆伝播
+	double dLdx_sum = 0;
 	
 	//領域確保
 	for (int i = 0; i < layer; i++) {
@@ -83,6 +108,9 @@ void learning(double t_in[], double t_out[],
 			x[i][j] = (double*)calloc(elenum, sizeof(double));
 		}
 	}
+	for (int i = 0; i < layer - 1; i++) {
+		dLdx[i] = (double*)calloc(elenum, sizeof(double));
+	}
 
 	//入力層から第1層への伝達
 	for (int i = 0; i < input; i++) {
@@ -90,39 +118,85 @@ void learning(double t_in[], double t_out[],
 			x[0][i][j] = t_in[i];
 		}
 	}
-	for (int i = 0; i < elenum; i++) {
+	for (int i = 0; i < elenum + 1; i++) {
 		for (int j = 0; j < elenum; j++) {
-			u[0][j] += t_in[i] * omega[0][i][j];
+			if (i == 0) u[0][j] += b * omega[0][i][j];
+			else u[0][j] += t_in[i - 1] * omega[0][i][j];
 		}
 	}
 
 	//中間層の伝達(3層以上の場合のみ)
 	if (layer >= 3) {
 		for (int i = 1; i < layer - 1; i++) {
-			for (int j = 0; j < elenum; j++) {
+			for (int j = 0; j < elenum + 1; j++) {
 				for (int k = 0; k < elenum; k++) {
-					u[i - 1][j] = sigmoid(u[i - 1][j]);
-					x[i][j][k] = u[i - 1][j];
-					u[i][k] += x[i][j][k] * omega[i][j][k];
+					if (j == 0) u[i][k] += b * omega[i][j][k];
+					else {
+						u[i - 1][j - 1] = sigmoid(u[i - 1][j - 1]);
+						x[i][j - 1][k] = u[i - 1][j - 1];
+						u[i][k] += x[i][j - 1][k] * omega[i][j][k];
+					}
 				}
 			}
 		}
 	}
 
 	//第(layer-1)層から出力層(第layer層)への伝達とNNの出力の計算
-
-	for (int j = 0; j < elenum; j++) {
+	for (int j = 0; j < elenum + 1; j++) {
 		for (int k = 0; k < output; k++) {
-			u[layer - 2][j] = sigmoid(u[layer - 2][j]);
-			x[layer - 1][j][k] = u[layer - 2][j];
-			y[k] += x[layer - 1][j][k] * omega[layer - 1][j][k];
+			if (j == 0) y[k] += b * omega[layer - 1][j][k];
+			else {
+				u[layer - 2][j - 1] = sigmoid(u[layer - 2][j - 1]);
+				x[layer - 1][j - 1][k] = u[layer - 2][j - 1];
+				y[k] += x[layer - 1][j - 1][k] * omega[layer - 1][j][k];
+			}
 		}
 	}
 	for (int k = 0; k < output; k++) {
 		y[k] = sigmoid(y[k]);
-		printf("%lf\n", y[k]);
 	}
 
+
+	//誤差逆伝播による重みの更新
+	//出力層
+	for (int i = 0; i < elenum + 1; i++) {
+		for (int j = 0; j < output; j++) {
+			if (i == 0) error = 2 * b * y[j] * (y[j] - t_out[j]) * (1 - y[j]);
+			else error = 2 * x[layer - 1][i - 1][j] * y[j] * (y[j] - t_out[j]) * (1 - y[j]);
+			omega[layer - 1][i][j] -= epsilon * error;
+			if (i > 0) dLdx[layer - 2][j] = 2 * omega[layer - 1][i][j] * y[j] * (y[j] - t_out[j]) * (1 - y[j]);
+		}
+	}
+
+	//中間層
+	for (int i = layer - 2; i > 0;i--) {
+		for (int j = 0; j < elenum + 1; j++) {
+			for (int k = 0; k < elenum; k++) {
+				for (int l = 0; l < elenum; l++) {
+					dLdx_sum += dLdx[i][l];
+				}
+				if (j == 0) error = b * u[i][k] * (1 - u[i][k]) * dLdx_sum;
+				else error = x[i][j - 1][k] * u[i][k] * (1 - u[i][k]) * dLdx_sum;
+				omega[i][j][k] -= epsilon * error;
+				dLdx_sum = 0;
+				dLdx[i - 1][k] *= omega[i][j][k] * u[i][k] * (1 - u[i][k]);
+			}
+		}
+	}
+
+	//入力層
+	for (int i = 0; i < input + 1; i++) {
+		for (int j = 0; j < elenum; j++) {
+			for (int k = 0; k < elenum; k++) {
+				dLdx_sum += dLdx[0][k];
+			}
+			if (i == 0) error = b * u[0][j] * (1 - u[0][j]) * dLdx_sum;
+			else error = x[0][i - 1][j] * u[0][j] * (1 - u[0][j]) * dLdx_sum;
+			omega[0][i][j] -= epsilon * error;
+		}
+		
+	}
+	
 	//領域の解放
 	for (int i = 0; i < layer; i++) {
 		for (int j = 0; j < elenum; j++) {
@@ -131,7 +205,11 @@ void learning(double t_in[], double t_out[],
 		free(x[i]);
 		if (i < layer - 1) free(u[i]);
 	}
+	for (int i = 0; i < layer - 1; i++) {
+		free(dLdx[i]);
+	}
 	free(x);
 	free(u);
 	free(y);
+	free(dLdx);
 }
